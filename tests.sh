ccf216cf58683b2fe6929dc0cc5fc87cfc693957
Leonardo Gazzarri <leogazza91@gmail.com>
2016-09-24 22:11:09 +0200

    big tests

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BIN1="./build/top_k_queries.tst1.bin 5"
-QUER="./test/test_cases/little_queries.txt"
+QUER="./test/test_cases/big_queries.txt"
 FILE="./data/enwiki-20160601-all-titles.nozero.unique.1.sdsl"
 
 perf record $BIN1 $QUER $FILE
