3f15d6fc93d467ffe208c07a77c01dddf1199412
Leonardo Gazzarri <leogazza91@gmail.com>
2016-09-26 20:20:58 +0200

    perf profiling

--- tests.sh
+++ tests.sh
@@ -4,11 +4,9 @@ DIR="./perf"
 BIN1="./build/top_k_queries.tst1.bin 5"
 BIN2="./build/top_k_queries.tst2.bin 5"
 QUER="./test/test_cases/medium_queries.txt"
-FILE="./data/enwiki-20160601-all-titles.nozero.unique.1.sdsl"
+FILE="./data/enwiki-20160601-all-titles"
+SUF1="1.sdsl"
+SUF2="2.sdsl"
 
-perf record -g -e cycles --output=$DIR/perf.record.tst1.data $BIN1 $QUER $FILE
-
-#perf record -g -e cycles --output=$DIR/perf.record.tst2.data $BIN2 $QUER $FILE
-
-#perf record -b --output=$DIR/perf.record.tst1.b.data $BIN1 $QUER $FILE
-#perf record -b --output=$DIR/perf.record.tst2.b.data $BIN2 $QUER $FILE
+perf record -g -e cycles --output=$DIR/perf.record.tst1.data $BIN1 $QUER $FILE.$SUF1
+perf record -g -e cycles --output=$DIR/perf.record.tst2.data $BIN2 $QUER $FILE.$SUF2
