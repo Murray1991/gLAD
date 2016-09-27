fc4e0ac7994e0cb97234ffa21609c05b6efd8032
Leonardo Gazzarri <leogazza91@gmail.com>
2016-09-27 21:47:51 +0200

    minor stuff

--- tests.sh
+++ tests.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+#perf stuff
+PERF_OPTS="-g --call-graph dwarf -F 90 -e cycles,instructions:u,L1-dcache-loads,L1-dcache-loads-misses,cache-references,cache-misses -o"
+PERF_REC="perf record"
+PERF="$PERF_REC $PERF_OPTS"
+
+#perf's files for report
 DIR="./perf"
+FILE_TST1="$DIR/perf.record.tst1.data"
+FILE_TST2="$DIR/perf.record.tst2.data"
+
+#perf1 & perf2
+PERF1="$PERF $FILE_TST1"
+PERF2="$PERF $FILE_TST2"
+
+#test's executables and parameters
 BIN1="./build/top_k_queries.tst1.bin 5"
 BIN2="./build/top_k_queries.tst2.bin 5"
-QUER="./test/test_cases/medium_queries.txt"
+QUER="./test/test_cases/medium_half_queries.txt"
+QUER2="./test/test_cases/big_queries.txt"
 FILE="./data/enwiki-20160601-all-titles"
 SUF1="1.sdsl"
 SUF2="2.sdsl"
 
-perf record -g -e cycles --output=$DIR/perf.record.tst1.data $BIN1 $QUER $FILE.$SUF1
-perf record -g -e cycles --output=$DIR/perf.record.tst2.data $BIN2 $QUER $FILE.$SUF2
+#profile the tests...
+#$PERF1 
+$BIN1 $QUER $FILE.$SUF1
+#$PERF2 
+$BIN2 $QUER $FILE.$SUF2
