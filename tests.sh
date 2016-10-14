78e6107dac3a6d053580537e6b6dbebdfd6ef683
Leonardo Gazzarri <leogazza91@gmail.com>
2016-10-14 17:17:57 +0200

    minor stuff in test

--- tests.sh
+++ tests.sh
@@ -10,25 +10,29 @@ DIR="./perf"
 FILE_TST1="$DIR/perf.record.tst1.data"
 FILE_TST2="$DIR/perf.record.tst2.data"
 FILE_TST3="$DIR/perf.record.tst3.data"
+FILE_TST4="$DIR/perf.record.tst4.data"
 
 #perf1 & perf2
 PERF1="$PERF $FILE_TST1"
 PERF2="$PERF $FILE_TST2"
 PERF3="$PERF $FILE_TST3"
+PERF4="$PERF $FILE_TST4"
 
 #test's executables and parameters
 BIN1="./build/top_k_queries.tst1.bin 5"
 BIN2="./build/top_k_queries.tst2.bin 5"
 BIN3="./build/top_k_queries.tst3.bin 5"
-QUER="./test/test_cases/medium_queries.txt"
-QUER2="./test/test_cases/big_queries.txt"
+BIN4="./build/top_k_queries.tst4.bin 5"
+QUER="./test/test_cases/very_little_queries.txt"
+QUER2="./test/test_cases/medium_queries.txt"
 FILE="./data/enwiki-20160601-all-titles"
 SUF1="1.sdsl"
 SUF2="2.sdsl"
 SUF3="3.sdsl"
+SUF4="4.sdsl"
 
 arg=$1
-[ -n "$arg" ] && PERF1="" && PERF2="" && PERF3="" && PERF_STAT="" && PERF_OPTS_STAT=""
+[ -n "$arg" ] && PERF1="" && PERF2="" && PERF3="" && PERF4="" && PERF_STAT="" && PERF_OPTS_STAT=""
 
 #profile the tests...
 EXE1="$PERF1 $BIN1 $QUER $FILE.$SUF1"
@@ -37,6 +41,8 @@ EXE2="$PERF2 $BIN2 $QUER $FILE.$SUF2"
 echo "$EXE2 ..."; $EXE2
 EXE3="$PERF3 $BIN3 $QUER $FILE.$SUF3"
 echo "$EXE3 ..."; $EXE3
+EXE4="$PERF4 $BIN4 $QUER $FILE.$SUF4"
+echo "$EXE4 ..."; $EXE4
 
 #perf stat
 PERF_OPTS_STAT="-e instructions:u,L1-dcache-loads,L1-dcache-loads-misses,cache-references,cache-misses "
