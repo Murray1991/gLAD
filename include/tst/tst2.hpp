0bf744aa6887497d4525552d2e9ee249a89c19d5
Leonardo Gazzarri <leogazza91@gmail.com>
2016-11-11 19:28:25 +0100

    minor fixes

--- include/tst/tst2.hpp
+++ include/tst/tst2.hpp
@@ -201,38 +201,6 @@ namespace glad {
             util::init_support(m_bp_sel10, &m_bp);
         }
         
-        /*
-        void build_tst_bp(tVS& strings, uint64_t N, uint64_t n, uint64_t max_weight) {
-            t_bv_uc                   start_bv(2*N+n+2, 0);
-            t_bv_uc                   helper(n+N,0);
-            int_vector<8> labels      = int_vector<8>(n);
-            m_bp                      = t_bv_uc(2*2*N, 0);
-            
-            bp_it                     = m_bp.begin();
-            start_it                  = start_bv.begin();
-            label_it                  = labels.begin();
-            helper_it                 = helper.begin();
-            *(start_it++) = 1;
-            
-            tnode * root  = build_tst(strings);
-            delete root;
-            
-            DEBUG_STDOUT("-- resizing...\n");
-            m_bp.resize(bp_it-m_bp.begin()); 
-            labels.resize(label_it-labels.begin()); 
-            helper.resize(helper_it-helper.begin());
-            start_bv.resize(start_it-start_bv.begin());
-            
-            DEBUG_STDOUT("-- building data structures...\n");
-            m_start_bv   = t_bv(start_bv);
-            m_helper     = t_bv(helper);
-            m_label      = t_label(labels);
-            m_start_sel  = t_sel(&m_start_bv);
-            m_bp_support = t_bp_support(&m_bp);
-            util::init_support(m_bp_rnk10, &m_bp);
-            util::init_support(m_bp_sel10, &m_bp);
-        } */
-        
         tnode * build_tst (tVS& strings) {
             int_t sx, dx; uint8_t ch;
             auto h_it = helper_it++;
@@ -285,97 +253,13 @@ namespace glad {
 
             return node;
         }
-        /*
-        tnode *build_tst (tVS& strings) 
-        {
-            typedef std::tuple<int_t, int_t, int_t, int_t, bool, tnode *, bool> call_t;
-            constexpr int_t target_level = 1; // >= 1 , with an higher number should be more efficient in memory consumption?
-            
-            std::stack<call_t> stk;
-            int_t sx, dx; char ch;
-            tnode * root = new tnode(), * node;
-            int_t first, last, index, level; bool help, marked;
-            
-            stk.emplace(0, strings.size()-1, 0, 0, 1, root, 0 <= target_level);
-            while ( ! stk.empty() ) 
-            {
-                std::tie(first, last, index, level, help, node, marked) = stk.top();
-                
-                if ( !marked ) {
-                    stk.pop();
-                }
-                if ( marked && !node->is_leaf() ) {
-                    stk.pop();
-                    if ( level < target_level )
-                        bp_it++;
-                    if ( level == target_level ) {
-                        compress(node);
-                        mark(node, help);
-                        delete node->hinode;
-                        delete node->eqnode;
-                        delete node->lonode;
-                        node->hinode = node->eqnode = node->lonode = nullptr;
-                    }
-                    continue;
-                }
-            
-                std::tie(sx, dx, ch) = partitionate(strings, first, last, index);
-                node->label = ch;
-                // put this if at the end, should be safe...
-                if ( level < target_level ) {
-                    start_it++;
-                    *(bp_it++)      = 1;
-                    *(start_it++)   = 1;
-                    *(label_it++)   = ch; 
-                }
-
-                if ( dx < last && !node->hinode ) {
-                    node->hinode = new tnode();
-                    stk.emplace(dx+1, last, index, level+1, 0, node->hinode, level+1 <= target_level);     //hinode
-                }
-                if ( sx <= dx && !node->eqnode ) {
-                    if ( sx < dx || (sx == dx && ch != EOS)) {
-                        node->eqnode = new tnode();
-                        stk.emplace(sx, dx, index+1, level+1, 1, node->eqnode, level+1 <= target_level);       //eqnode
-                    }
-                    if (sx == dx && ch == EOS) {
-                        if ( marked ) {
-                            if ( level < target_level )
-                                bp_it++;
-                            else {
-                                mark(node,help);
-                            }
-                            stk.pop();
-                        }
-                        // clear string added
-                        strings[sx].clear();
-                    }
-                }
-                if ( first < sx && !node->lonode ) {
-                    node->lonode = new tnode();
-                    stk.emplace(first, sx-1, index, level+1, 0, node->lonode, level+1 <= target_level);    //lonode
-                }
-                // put this if at the end, should be safe...
-                if ( level < target_level ) {
-
-                    //TODO check!
-                    if ( ! node->is_leaf() )
-                        *(helper_it++)  = ( node->lonode != nullptr );
-
-                }
-            }
-            return root;
-        }*/
         
         void mark(tnode * node, bool help) {
             if ( node == nullptr )
                 return;
             for(auto it = node->label.begin(); it != node->label.end(); (*(label_it++) = *(it++)), start_it++);
-
-            //TODO check!
             if ( !node->is_leaf() )
                 *(helper_it++) = ( node->lonode != nullptr );
-
             *(start_it++)   = 1;
             *(bp_it++)      = 1;
             mark(node->lonode, 0);
@@ -432,12 +316,11 @@ namespace glad {
         
         D ( __attribute__((noinline)) )
         int64_t map_to_edge(size_t v, uint8_t ch1, uint8_t ch2) const {
-            //problem here: children(v) calls find_close
+            //problem here: children(v) calls repeatedly find_close
             auto nodes = children(v);
             auto out_size = nodes.size();
             auto d = m_bp_rnk10(v+1);
             auto h = m_helper[node_id(v)-1-d];
-            //cout << "map_to_edge: " << v << " ( " << nodes << " ) ; id:" << node_id(v) << " ; " << ch1 << " ; " << ch2 << " ; h=" << h << " ; d=" << d << endl;
             if (out_size == 3)
                 return nodes[ (ch1==ch2) + (ch1>ch2)*2 ];
             else if (out_size == 2 && h == 1 && ch1 <= ch2)
@@ -525,27 +408,6 @@ namespace glad {
                 str.pop_back();
             return str;
         }
-        
-        /* build string from node v_from upwards to node v_to (v_to is the parent of the node found via blind search or 0)*/
-        /*D ( __attribute__((noinline)) )
-        std::string build_string(size_t v_from, size_t v_to) const {
-            const char * data = (const char *) m_label.data();
-            std::string str = "";
-            size_t v = v_from;
-            size_t i, o, p;
-            bool b = true;
-            #pragma ivdep
-            for ( b = true; v != v_to ; ) {
-                i = get_start_label(v);
-                o = get_end_label(v);
-                std::string lab(data+i, o - i - !b);    //at the beginning is true => start from a leaf!
-                str = std::move(lab) + str;
-                p = parent(v);
-                b = check_if_eqnode(v,p);               //check if this node for the parent is the eqnode
-                v = p;                                  //go up!
-            }
-            return str;
-        }*/
         
         D ( __attribute__((noinline)) )
         t_range prefix_range(const int64_t& v) const {
