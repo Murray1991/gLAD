4745de60ee2124708581993cf6d824a485971a67
Leonardo Gazzarri <leogazza91@gmail.com>
2016-11-12 17:38:54 +0100

    minor fix

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ ! $# -gt 0 ] && echo "Usage: ./tests.sh k [record|stat]" && exit 1
+[ ! $# -gt 0 ] && echo "Usage: ./tests.sh K [record|stat]" && exit 1
 
 #perf directory
 DIR="./perf"
@@ -20,31 +20,37 @@ PERF_OPTS_STAT="-e instructions:u,L1-dcache-loads,L1-dcache-loads-misses,cache-r
 
 #executables and parameters
 FILE="./data/enwiki-20160601-all-titles"
-QUER="./test/test_cases/medium_queries.txt"
-
 K=$1
-EXE1="./build/top_k_queries.tst1.bin $K $QUER $FILE.1.sdsl"
-EXE2="./build/top_k_queries.tst2.bin $K $QUER $FILE.2.sdsl"
-EXE3="./build/top_k_queries.tst3.bin $K $QUER $FILE.3.sdsl"
-EXE4="./build/top_k_queries.tst4.bin $K $QUER $FILE.4.sdsl"
-
 ARG=$2
-if [ "$ARG" == 'record' ]
-then
-    EXE1="$PERF1 $EXE1"
-    EXE2="$PERF2 $EXE2"
-    EXE3="$PERF3 $EXE3"
-    EXE4="$PERF4 $EXE4"
-elif [ "$ARG" == 'stat' ]
-then
-    EXE1="$PERF_STAT $PERF_OPTS_STAT $EXE1"
-    EXE2="$PERF_STAT $PERF_OPTS_STAT $EXE2"
-    EXE3="$PERF_STAT $PERF_OPTS_STAT $EXE3"
-    EXE4="$PERF_STAT $PERF_OPTS_STAT $EXE4"
-fi
 
-#tests...
-echo "$EXE1 ..."; $EXE1
-echo "$EXE2 ..."; $EXE2
-echo "$EXE3 ..."; $EXE3
-echo "$EXE4 ..."; $EXE4
+function execute {
+    QUER=$1
+    EXE1="./build/top_k_queries.tst1.bin $K $QUER $FILE.1.sdsl"
+    EXE2="./build/top_k_queries.tst2.bin $K $QUER $FILE.2.sdsl"
+    EXE3="./build/top_k_queries.tst3.bin $K $QUER $FILE.3.sdsl"
+    EXE4="./build/top_k_queries.tst4.bin $K $QUER $FILE.4.sdsl"
+
+    if [ "$ARG" == 'record' ]
+    then
+        EXE1="$PERF1 $EXE1"
+        EXE2="$PERF2 $EXE2"
+        EXE3="$PERF3 $EXE3"
+        EXE4="$PERF4 $EXE4"
+    elif [ "$ARG" == 'stat' ]
+    then
+        EXE1="$PERF_STAT $PERF_OPTS_STAT $EXE1"
+        EXE2="$PERF_STAT $PERF_OPTS_STAT $EXE2"
+        EXE3="$PERF_STAT $PERF_OPTS_STAT $EXE3"
+        EXE4="$PERF_STAT $PERF_OPTS_STAT $EXE4"
+    fi
+
+    #tests...
+    echo "$EXE1 ..."; $EXE1
+    echo "$EXE2 ..."; $EXE2
+    echo "$EXE3 ..."; $EXE3
+    echo "$EXE4 ..."; $EXE4
+}
+
+#execute "./test/test_cases/big_range_queries1.txt"
+#execute "./test/test_cases/big_range_queries2.txt"
+execute "./test/test_cases/range_queries.txt"
