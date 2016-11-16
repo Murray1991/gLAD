f79f42a4ce58fb3a005cf884697c79407981b94d
Leonardo Gazzarri <leogazza91@gmail.com>
2016-11-16 10:32:56 +0100

    avg values for top-k query

--- testK.sh
+++ testK.sh
@@ -2,10 +2,23 @@
 
 [ ! $# -gt 0 ] && echo "Usage: ./testK.sh indexNumber queryFile file" && exit 1
 
+function avg {
+    res=$(printf $1 | awk '{ sum += $1; n++ } END { if (n > 0) print sum / n; }')
+    echo $res
+}
+
 function execute {
-    K=$1; N=$2; Q=$3; I=$4.$2.sdsl;
+    K=$1; N=$2; Q=$3; I=$4.$2.sdsl;col="";
     EXE="./build/top_k_queries.tst$N.bin $K $Q $I"
-    $EXE
+    for i in `seq 1 5`;
+    do
+        res=`$EXE`
+        arr=($res);
+        col="${arr[1]}\n$col"
+        echo "# $res"
+    done
+    val=$(avg $col)
+    echo "$K $val"
 }
 
 echo "### indexNumber=$1, file= queryFile=$2"
