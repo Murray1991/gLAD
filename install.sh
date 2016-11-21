091518d265f3ab89a4e438a3a4932322be668f12
Leonardo Gazzarri <leogazza91@gmail.com>
2016-11-21 19:05:46 +0100

    final version

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ DIRECTORY=./external
 [ ! -d $DIRECTORY ] && mkdir $DIRECTORY
 git clone https://github.com/simongog/sdsl-lite.git $DIRECTORY/sdsl-lite
 $DIRECTORY/sdsl-lite/install.sh ./sdsl
-rm -r $DIRECTORY
+rm -rf $DIRECTORY
